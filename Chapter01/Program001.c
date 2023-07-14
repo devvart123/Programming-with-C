70c267c4c3574ebca2b86b7350719584c764769a
devvart123 <130150566+devvart123@users.noreply.github.com>
2023-07-14 08:03:53 +0530

    First Commit Program 1

---
+++ Chapter01/Program001.c
@@ -0,0 +1,9 @@
+//Lecture 1 Write a Program to Print Hello World using C Language
+#include<stdio.h>
+
+int main(){
+    printf("Hello World, Lets start to Learn C Language");
+    printf("This is 1st Program");
+    
+    return 0;
+}
